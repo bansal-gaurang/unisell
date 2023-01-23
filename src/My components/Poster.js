a0026debb0b228e872e79b84ccc5affbe93d615a
bansal-gaurang <bansalofficial0001@gmail.com>
2023-01-23 11:36:49 +0530

    deploying

--- src/My components/Poster.js
+++ src/My components/Poster.js
@@ -1,4 +1,4 @@
-import pic from "/home/bansal/Desktop/unisell/src/My components/vit.jpg";
+import pic from "src/My components/vit.jpg";
 function poster(){
  return (<img style={{ width: 1300, height: 420 }} src={pic} alt="vit logo"/>
 
